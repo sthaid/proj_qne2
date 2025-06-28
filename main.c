b864c665b9126243e232e7fd5b4e3595f1334236
Steven Haid <stevenhaid@gmail.com>
2025-06-28 17:43:43 -0400

    cont

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <common.h>
 
+// xxx update common.h
+
 #include <SDL.h>
 
 #include <sys/socket.h>
@@ -8,9 +10,6 @@
 
 #include <sys/types.h>
 #include <dirent.h>
-
-
-
 
 void *server_thread(void *cx);
 
@@ -47,54 +46,47 @@ int SDL_main(int argc, char **argv)
     return 0;
 }
 
-const char *int_storage_path;
+// ----------------- SERVER ----------------------------
 
+// variables
+const char *internal_storage_path;
+
+// prototypes
+static void process_client_req(int sockfd);
+static void list_internal_storage_files(void);
 static char * sock_addr_to_str(char * s, int slen, struct sockaddr * addr);
-static void process(int sockfd);
-
-static void list_files(void)
-{
-    DIR *d = opendir(int_storage_path);
-    if (d == NULL) {
-        ERROR("opendir failed, %s\n", strerror(errno));
-        return;
-    }
-
-    struct dirent * dirent;
-    while ((dirent = readdir(d)) != NULL) {
-        INFO("XXX %s\n", dirent->d_name);
-    }
-
-    closedir(d);
-}
+static void remove_trailing_crlf(char *s);
 
 void *server_thread(void *cx)
 {
     struct sockaddr_in server_address;
     int                listen_sockfd;
     int                ret;
-    unsigned short     port = 1234;
+    unsigned short     port = 1234; //xxx use define, and/or make adjustable
 
     INFO("SERVER_THREAD STARTING\n");
 
-    int_storage_path = SDL_AndroidGetInternalStoragePath();
-    INFO("int_storage_path = %s\n", int_storage_path);
-    list_files();
+    // xxx
+    internal_storage_path = SDL_AndroidGetInternalStoragePath();
+    INFO("internal_storage_path = %s\n", internal_storage_path);
 
-    // create socket
+    // xxx
+    list_internal_storage_files();
+
+    // create listen socket
     listen_sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (listen_sockfd == -1) {
         ERROR("socket, %s\n", strerror(errno));
     }
 
-    // set socket options
+    // set socket options  xxx what is this for
     int reuseaddr = 1;
     ret = setsockopt(listen_sockfd, SOL_SOCKET, SO_REUSEADDR, (const char*)&reuseaddr, sizeof(reuseaddr));
     if (ret == -1) {
         ERROR("setsockopt SO_REUSEADDR, %s", strerror(errno));
     }
 
-    // bind socket
+    // bind socket to any ip addr, for specified port
     memset(&server_address, 0, sizeof(server_address));
     server_address.sin_family      = AF_INET;
     server_address.sin_addr.s_addr = INADDR_ANY;
@@ -103,7 +95,7 @@ void *server_thread(void *cx)
                (struct sockaddr *)&server_address,
                sizeof(server_address));
     if (ret == -1) {
-        ERROR("bind");
+        ERROR("bind");  // xxx newlines
     }
 
     // listen 
@@ -134,7 +126,7 @@ void *server_thread(void *cx)
         INFO("accepted connection from %s\n", peer_addr_str);
 
         // xxx
-        process(sockfd);
+        process_client_req(sockfd);
 
         // xxx
         close(sockfd);
@@ -145,74 +137,110 @@ void *server_thread(void *cx)
     return NULL;
 }
 
-static void remove_trailing_crlf(char *s)
+// xxx check sockfp, and other checks
+static void process_client_req(int sockfd)
 {
-    int len = strlen(s);
+    FILE *sockfp=NULL;
+    char  password[100], cmd[100], arg1[100], arg2[100];
+    char  err_str[100]="";
 
-    if (len > 0 && (s[len-1] == '\n' || s[len-1] == '\r')) {
-        s[len-1] = '\0';
-        len--;
-    } else {
+    // xxx
+    sockfp = fdopen(sockfd, "r+");
+    if (sockfp == NULL) {
+        ERROR("failed to fdopen sockfp, %s\n", strerror(errno));
         return;
     }
 
-    if (len > 0 && (s[len-1] == '\n' || s[len-1] == '\r')) {
-        s[len-1] = '\0';
-        len--;
+    // read first line from client
+    password[0] = cmd[0] = arg1[0] = arg2[0] = '\0';
+    if (fscanf(sockfp, "password=%s cmd=%s arg1=%s arg2=%s", password, cmd, arg1, arg2) < 2) {
+        sprintf(err_str, "bad request");
+        goto error;
     }
+
+    // validate password xxx tbd
+    if (strcmp(password, "none") != 0) {
+        sprintf(err_str, "invalid password");
+        goto error;
+    }
+
+    // process cmd
+    INFO("processing cmd=%s arg1=%s arg2=%s\n", cmd, arg1, arg2);
+    if (strcmp(cmd, "file_put") == 0) {
+        // copy file to internal storage
+        FILE *fp;
+        char  filename[100], s[1000];
+
+        if (arg1[0] == '\0') {
+            sprintf(err_str, "file_put, filename not provied\n");
+            goto error;
+        }
+
+        sprintf(filename, "%s/%s", internal_storage_path, arg1);
+        fp = fopen(filename, "w");
+        if (fp == NULL) {
+            sprintf(err_str, "failed to open %s for writing, %s\n", filename, strerror(errno));
+            goto error;
+        }
+        while (fgets(s, sizeof(s), sockfp) != NULL) {
+            fputs(s, fp);
+        }
+        fclose(fp);
+    } else if (strcmp(cmd, "file_get") == 0) {
+        // print contents of internal storage file
+        FILE *fp;
+        char  filename[100], s[1000];
+
+        if (arg1[0] == '\0') {
+            sprintf(err_str, "file_get, filename not provied\n");
+            goto error;
+        }
+
+        sprintf(filename, "%s/%s", internal_storage_path, arg1);
+        fp = fopen(filename, "r");
+        if (fp == NULL) {
+            sprintf(err_str, "failed to open %s for reading, %s\n", filename, strerror(errno));
+            goto error;
+        }
+        while (fgets(s, sizeof(s), fp) != NULL) {
+            fputs(s, sockfp);
+        }
+        fclose(fp);
+    } else if (strcmp(cmd, "file_rm") == 0) {
+        // remove internel storage file
+    } else if (strcmp(cmd, "file_ls") == 0) {
+        // list files in internal storage
+    } else {
+        // invalid command
+    }
+
+    fprintf(sockfp, "OKAY\n");
+    fclose(sockfp);
+    return;
+
+error:
+    fprintf(sockfp, "%s\n", err_str);
+    fclose(sockfp);
+    return;
 }
 
-static void process(int sockfd)
+// ----------------- SUPPORT ---------------------------
+
+static void list_internal_storage_files(void)
 {
-    char s[1000], password[100], cmd[100], arg1[100], arg2[100], arg3[100];
-    FILE *fpin=NULL, *fp=NULL;
-    int line=0;
+    DIR *dir = opendir(internal_storage_path);
+    struct dirent * dirent;
 
-    fpin = fdopen(sockfd, "r+");
-    // xxx check fpin, and other checks
-    while (fgets(s, sizeof(s), fpin) != NULL) {
-        line++;
-        remove_trailing_crlf(s);
-        INFO("XXX %d: '%s'\n", line, s);
-
-        if (line == 1) {
-            password[0] = cmd[0] = arg1[0] = arg2[0] = arg3[0] = '\0';
-            if (sscanf(s, "password=%s cmd=%s arg1=%s arg2=%s arg3=%s", password, cmd, arg1, arg2, arg3) < 2) {
-                ERROR("cmd line expected, got '%s'\n", s);
-                break;
-            }
-            if (strcmp(password, "none") != 0) {
-                ERROR("password invalid: got '%s', expected '%s'\n", password, "none");
-                break;
-            }
-            INFO("password=%s cmd=%s arg1=%s arg2=%s arg3=%s\n", password, cmd, arg1, arg2, arg3);
-            continue;
-        }
-
-        if (strcmp(cmd, "file_put") == 0) {
-            char filename[200];
-            sprintf(filename, "%s/%s", int_storage_path, arg1);
-
-            // xxx check arg
-            if (line == 2) {
-                fp = fopen(filename, "w");
-            }
-            fprintf(fp, "%s\n", s);
-        }
+    if (dir == NULL) {
+        ERROR("opendir failed, %s\n", strerror(errno));
+        return;
     }
 
-    INFO("OKAY OKAY\n");
-    fprintf(fpin, "OKAY\n");
-    fflush(fpin);
+    while ((dirent = readdir(dir)) != NULL) {
+        INFO("%s\n", dirent->d_name);  // xxx other fields too
+    }
 
-    if (fpin) {
-        fclose(fpin);
-        fpin = NULL;
-    }
-    if (fp) {
-        fclose(fp);
-        fp = NULL;
-    }
+    closedir(dir);
 }
 
 static char * sock_addr_to_str(char * s, int slen, struct sockaddr * addr)
@@ -239,3 +267,19 @@ static char * sock_addr_to_str(char * s, int slen, struct sockaddr * addr)
     return s;
 }
 
+static void remove_trailing_crlf(char *s)
+{
+    int len = strlen(s);
+
+    if (len > 0 && (s[len-1] == '\n' || s[len-1] == '\r')) {
+        s[len-1] = '\0';
+        len--;
+    } else {
+        return;
+    }
+
+    if (len > 0 && (s[len-1] == '\n' || s[len-1] == '\r')) {
+        s[len-1] = '\0';
+        len--;
+    }
+}
