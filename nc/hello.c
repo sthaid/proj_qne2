e86e31749a816ba397ddae75100cc4f5f00a225d
Steven Haid <stevenhaid@gmail.com>
2025-07-05 10:52:21 -0400

    cont

--- nc/hello.c
+++ nc/hello.c
@@ -5,33 +5,11 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-#include <picoc_unix.h>
-
-typedef short int16_t;
-
-typedef struct {
-    int16_t x;
-    int16_t y;
-    int16_t w;
-    int16_t h;
-} rect_t;
-
-rect_t proc(void)
-{
-    rect_t r;
-    r.x = 1;
-    r.y = 2;
-    r.w = 3;
-    r.h = 4;
-    return r;
-}
-
-#define TEST_VAL 567801
+#include <sdl.h>
 
 int main(int argc, char **argv)
 {
     time_t t;
-    rect_t rect;
     struct sdl_rect *loc;
 
     printf("sizoef(char)      = %zd\n", sizeof(char));
@@ -44,30 +22,16 @@ int main(int argc, char **argv)
     printf("sizeof(123)       = %zd\n", sizeof(123));
     printf("sizeof(123UL)     = %zd\n", sizeof(123UL));
 
-    printf("sizeof sdl_rect = %zd\n", sizeof(struct sdl_rect));
-
-    printf("calling test2\n");
-    loc = test2();
-    printf("%d %d %d %d\n", 
-       loc->x, loc->y, loc->w, loc->h);
-
-    printf("calling test\n");
-    test(TEST_VAL);
-
-    printf("COLOR_YELLOW = 0x%x\n", COLOR_YELLOW);
-
     printf("calling display init\n");
     sdl_display_init(COLOR_YELLOW);
 
     printf("calling sdl_render_text\n");
     loc = sdl_render_text(100, 200, "hello world");
-    printf("%d %d %d %d\n", 
-       loc->x, loc->y, loc->w, loc->h);
+    printf("%d %d %d %d\n", loc->x, loc->y, loc->w, loc->h);
 
     printf("calling sdl_render_printf\n");
     loc = sdl_render_printf(100, 400, "%s %d %d", "hello", 1, 2);
-    printf("%d %d %d %d\n", 
-       loc->x, loc->y, loc->w, loc->h);
+    printf("%d %d %d %d\n", loc->x, loc->y, loc->w, loc->h);
 
     printf("calling display present\n");
     sdl_display_present();
@@ -76,22 +40,5 @@ int main(int argc, char **argv)
     sleep(3);
 
     printf("return\n");
-    return 0;
-
-    rect = proc();
-    printf("%d %d %d %d\n",
-       rect.x, rect.y, rect.w, rect.h);
-    return 0;
-
-    while (true) {
-        time(&t);
-        printf("hello %d %d %lld\n", t, 123, 1234567890000UL);
-
-        sleep(1);
-
-        time(&t);
-        printf("bye %d\n", t);
-    }
-
     return 0;
 }
--- picoc/platform/library_unix.c
+++ picoc/platform/library_unix.c
@@ -1,78 +1,6 @@
 #include "../interpreter.h"
 #include "../../sdl.h"
 
-void UnixSetupFunc(Picoc *pc)
-{
-    printf("XXXXXXX UnixSetupFunc ...\n");
-
-// TypeCreateOpaqueStruct(pc, NULL, TableStrRegister(pc, "sdl_rect"),
-//      sizeof(struct sdl_rect));
-
-
-    // xxx or move to bottom
-    #define XXX(name, type, writeable) \
-        VariableDefinePlatformVar(pc, \
-                                  NULL, \
-                                  #name, \
-                                  &pc->type,\
-                                  (union AnyValue*)&name,\
-                                  writeable);
-        
-    //XXX(COLOR_YELLOW, IntType, false);
-
-    //VariableDefinePlatformVar(pc, NULL, "COLOR_YELLOW", &pc->IntType,
-        //(union AnyValue*)&COLOR_YELLOW, false);
-}
-
-void Ctest (struct ParseState *Parser, struct Value *ReturnValue,
-	struct Value **Param, int NumArgs)
-{
-    printf("test(%d)\n", Param[0]->Val->Integer);
-}
-
-void Clineno (struct ParseState *Parser, struct Value *ReturnValue,
-	struct Value **Param, int NumArgs)
-{
-    ReturnValue->Val->Integer = Parser->Line;
-}
-
-void Sdl_display_init (struct ParseState *Parser, struct Value *ReturnValue,
-	struct Value **Param, int NumArgs)
-{
-    int color = Param[0]->Val->Integer;
-    sdl_display_init(color);
-}
-
-void Sdl_display_present (struct ParseState *Parser, struct Value *ReturnValue,
-	struct Value **Param, int NumArgs)
-{
-    sdl_display_present();
-}
-
-void Test2 (struct ParseState *Parser, struct Value *ReturnValue,
-	struct Value **Param, int NumArgs)
-{
-    static struct sdl_rect loc = {1,2,3,7};
-
-    ReturnValue->Val->Pointer = &loc;
-}
-
-void Sdl_render_text (struct ParseState *Parser, struct Value *ReturnValue,
-	struct Value **Param, int NumArgs)
-{
-    static struct sdl_rect loc;
-
-    int x = Param[0]->Val->Integer;
-    int y = Param[1]->Val->Integer;
-    char *str = Param[2]->Val->Identifier;
-
-    printf("XXX GOT %d %d %s\n", x, y, str);
-
-    loc = *sdl_render_text(x, y, str);
-
-    ReturnValue->Val->Pointer = &loc;
-}
-
 struct StdVararg
 {
     struct Value **Param;
@@ -82,50 +10,91 @@ struct StdVararg
 int StdioBasePrintf(struct ParseState *Parser, FILE *Stream, char *StrOut,
     int StrOutLen, char *Format, struct StdVararg *Args);
 
+// -----------------  SDL PLATFORM ROUTINES  ----------------------------
+
+void Sdl_display_init (struct ParseState *Parser, struct Value *ReturnValue,
+	struct Value **Param, int NumArgs)
+{
+    int color = Param[0]->Val->Integer;
+
+    sdl_display_init(color);
+}
+
+void Sdl_display_present (struct ParseState *Parser, struct Value *ReturnValue,
+	struct Value **Param, int NumArgs)
+{
+    sdl_display_present();
+}
+
+void Sdl_render_text (struct ParseState *Parser, struct Value *ReturnValue,
+	struct Value **Param, int NumArgs)
+{
+    int               x   = Param[0]->Val->Integer;
+    int               y   = Param[1]->Val->Integer;
+    char            * str = Param[2]->Val->Identifier;
+    struct sdl_rect * loc;
+
+    loc = sdl_render_text(x, y, str);
+
+    ReturnValue->Val->Pointer = loc;
+}
+
 void Sdl_render_printf (struct ParseState *Parser, struct Value *ReturnValue,
 	struct Value **Param, int NumArgs)
 {
-    struct StdVararg PrintfArgs;
-    int x =  Param[0]->Val->Integer;
-    int y =  Param[1]->Val->Integer;
-    char *fmt = Param[2]->Val->Identifier;
-    char str[200] = "";
-
-    static struct sdl_rect loc;
+    int               x   =  Param[0]->Val->Integer;
+    int               y   =  Param[1]->Val->Integer;
+    char            * fmt = Param[2]->Val->Identifier;
+    struct StdVararg  PrintfArgs;
+    char              str[200] = "";
+    struct sdl_rect * loc;
 
     PrintfArgs.Param = Param + 2;
     PrintfArgs.NumArgs = NumArgs - 3;
-
     StdioBasePrintf(Parser, NULL, str, sizeof(str), fmt, &PrintfArgs);
 
-    printf("XXXXX NumArgs=%d xy=%d %d fmt=%s str=%s\n", NumArgs, x, y, fmt, str);
+    loc = sdl_render_text(x, y, str);
 
-    loc = *sdl_render_text(x, y, str);
-
-    ReturnValue->Val->Pointer = &loc;
+    ReturnValue->Val->Pointer = loc;
 }
 
-/* list of all library functions and their prototypes */
-struct LibraryFunction UnixFunctions[] =
-{
-    {Ctest, "void test(int);"},
-    {Clineno, "int lineno();"},
+// -----------------  SDL PLATFORM REGISTRATION -------------------------
 
-    { Sdl_display_init, "void sdl_display_init(int color);" },
+struct LibraryFunction SdlFunctions[] = {
+    { Sdl_display_init,    "void sdl_display_init(int color);" },
     { Sdl_display_present, "void sdl_display_present(void);" },
-    { Sdl_render_text, "struct sdl_rect *sdl_render_text(int x, int y, char * str);" },
-    { Sdl_render_printf, "struct sdl_rect *sdl_render_printf(int x, int y, char * fmt, ...) ;" },
-
-    { Test2, "struct sdl_rect *test2(void);" },
-
-    {NULL, NULL}
-};
+    { Sdl_render_text,     "struct sdl_rect *sdl_render_text(int x, int y, char * str);" },
+    { Sdl_render_printf,   "struct sdl_rect *sdl_render_printf(int x, int y, char * fmt, ...) ;" },
+    { NULL, NULL } };
 
 void PlatformLibraryInit(Picoc *pc)
 {
-    XXX(COLOR_YELLOW, IntType, false);
+    #define DEFINE_PLATFORM_VAR(name, type, writeable) \
+        VariableDefinePlatformVar(pc, \
+                                  NULL, \
+                                  #name, \
+                                  &pc->type,\
+                                  (union AnyValue*)&name,\
+                                  writeable);
 
-    //IncludeRegister(pc, "picoc_unix.h", &UnixSetupFunc, &UnixFunctions[0], NULL);
-    IncludeRegister(pc, "picoc_unix.h", &UnixSetupFunc, &UnixFunctions[0], 
-      "struct sdl_rect {short x; short y; short w; short h;};" );
+    // sdl - platform init
+    DEFINE_PLATFORM_VAR(COLOR_PURPLE, IntType, false);
+    DEFINE_PLATFORM_VAR(COLOR_BLUE, IntType, false);
+    DEFINE_PLATFORM_VAR(COLOR_LIGHT_BLUE, IntType, false);
+    DEFINE_PLATFORM_VAR(COLOR_GREEN, IntType, false);
+    DEFINE_PLATFORM_VAR(COLOR_YELLOW, IntType, false);
+    DEFINE_PLATFORM_VAR(COLOR_ORANGE, IntType, false);
+    DEFINE_PLATFORM_VAR(COLOR_PINK, IntType, false);
+    DEFINE_PLATFORM_VAR(COLOR_RED, IntType, false);
+    DEFINE_PLATFORM_VAR(COLOR_GRAY, IntType, false);
+    DEFINE_PLATFORM_VAR(COLOR_WHITE, IntType, false);
+    DEFINE_PLATFORM_VAR(COLOR_BLACK, IntType, false);
+
+    IncludeRegister(
+        pc, 
+        "sdl.h", 
+        NULL,  // SetupFunction not used
+        SdlFunctions, 
+        "struct sdl_rect {short x; short y; short w; short h;};"
+                    );
 }
