f28f50050ccecf3705368caa000c1141f8a23ddf
Steven Haid <stevenhaid@gmail.com>
2025-12-11 08:26:11 -0500

    cont

--- files/apps/Tennis/tennis.c
+++ files/apps/Tennis/tennis.c
@@ -1,3 +1,31 @@
+/* TODO FIRST
+
+vary serve
+- position 
+- direction
+
+speeds up when motion is active 
+
+limit paddle motion to x span
+
+*/
+
+
+/* TODO
+speed up ball gradually
+
+computer simulation improvement
+
+params, such as 
+- disable sound
+- initial speed control
+- max speed
+
+delay before serve
+
+indicate which score is computer vs human
+*/
+
 #include <stdio.h>
 #include <stdbool.h>
 #include <math.h>
@@ -7,38 +35,37 @@
 #include <utils.h>
 
 // defines
-#define INTERVAL 10000
-#define SIZE 100
+#define DEG2RAD              (M_PI/180.0)
+#define UPDATE_INTERVAL_USEC 10000  // 10 ms
+#define BALL_RADIUS          50
+#define PADDLE_W             300
+#define PADDLE_H             50
 
 // variables
-char *progname;
-char *data_dir;
+char  *progname;
+char  *data_dir;
 
-double x, y, vx, vy;
-double x_last, y_last;
+double y_top, y_bottom;
+double x, y, x_last, y_last;
+double vx, vy;
+double human_paddle_x, human_paddle_y;
+double computer_paddle_x, computer_paddle_y;
+int    human_score, computer_score;
+bool   serve_needed;
 
-int paddle_x, paddle_y;
+// prototypes    
+void computer_paddle_control(void);
+void bounce_ball_off_paddle(bool is_human_paddle);
+void play_tone(int freq, int duration_ms);
 
-int computer_paddle_x, computer_paddle_y;
-
-int computer_score, human_score;
-
-#define PADDLE_W  300
-#define PADDLE_H  50
-    
-void paddle(void);
-void computer_paddle(void);
-void play_tone(int freq, int intvl_ms);
-
-// -----------------  MAIN  ------------------------------------------
+// -----------------  MAIN  -----------------------
     
 int main(int argc, char **argv)
 {
-    int          rc;
-    sdlx_event_t event;
-    bool         done = false;
-
-    sdlx_texture_t *ball;
+    int             rc;
+    sdlx_event_t    event;
+    bool            end_program = false;
+    sdlx_texture_t *ball = NULL;
 
     // save args
     if (argc != 2) {
@@ -56,142 +83,138 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    computer_score = human_score = 0;
-    ball = sdlx_create_filled_circle_texture(SIZE/2, COLOR_GREEN);
-    x = sdlx_win_width/2;
-    y = 0;
-    vx = 0;
-    vy = 10;
-    paddle_x = sdlx_win_width/2;
-    paddle_y = sdlx_win_height - 200;
-
+    // initialization:
+    // - create ball texture
+    // - set scores to 0
+    // - set the top and bottom of the display region used by the game
+    // - set initial paddle locations
+    // - set serve_needed flag
+    ball = sdlx_create_filled_circle_texture(BALL_RADIUS, COLOR_GREEN);
+    computer_score    = 0;
+    human_score       = 0;
+    y_top             = 0;
+    y_bottom          = sdlx_win_height - 200;
+    human_paddle_x    = sdlx_win_width/2;
+    human_paddle_y    = y_bottom - 200;
     computer_paddle_x = sdlx_win_width/2;
-    computer_paddle_y = 200;
+    computer_paddle_y = y_top + 200;
+    serve_needed      = true;
 
+    // xxx
+    x = sdlx_win_width/2;
+
+    // init large font;
+    // LARGE_FONT is defined as '10' which means 10 chars across the display
     sdlx_print_init_numchars(LARGE_FONT);
 
-    play_tone(500,250);
-    sleep(1);
-    play_tone(1000,100);
-    sleep(1);
-
     // runtime loop
-    while (!done) {
+    while (!end_program) {
         // init the backbuffer
         sdlx_display_init(COLOR_BLACK);
 
-        // ball
-        //sdlx_render_fill_rect(x-SIZE/2, y-SIZE/2, SIZE, SIZE, COLOR_WHITE);
-        sdlx_render_texture(x-SIZE/2, y-SIZE/2, SIZE, SIZE, ball);
-
-
-
-        // bounce off of walls
-        if (y > sdlx_win_height) {
-            printf("comptuer scored, y=%f\n", y);
-            x = sdlx_win_width/2;
-            y = 0;
+        // if serve needed then reset variables
+        // xxx vary the serve
+        // xxx define for serve velocity
+        if (serve_needed) {
+            //x = sdlx_win_width/2;
+            //y = 0;
+            // xxx vary direction
+            y = computer_paddle_y + PADDLE_H/2 + BALL_RADIUS;
             vx = 0;
             vy = 10;
             x_last = x;
             y_last = y;
-            computer_score++;
-            play_tone(500,250);
-        } else if (y < 0) {
-            printf("human scored, y=%f\n", y);
-            x = sdlx_win_width/2;
-            y = 0;
-            vx = 0;
-            vy = 10;
-            x_last = x;
-            y_last = y;
-            human_score++;
-            play_tone(500,250);
-        } else {
-            if (x < 0 || x > sdlx_win_width) vx = -vx;
-            //if (y < 0 || y > sdlx_win_height) vy = -vy;
-            x_last = x;
-            y_last = y;
+            serve_needed = false;
+        }
+
+        // update ball position, using the ball x,y velocity
+        x_last = x;
+        y_last = y;
+        x += vx;
+        y += vy;
+
+        // bounce ball off of side walls
+        if (x < 0 || x > sdlx_win_width) {
+            vx = -vx;
             x += vx;
-            y += vy;
         }
 
+        // computer paddle control
+        computer_paddle_control();
 
-        // bounce off of paddle
-        if (y_last+SIZE/2 < paddle_y && 
-            y+SIZE/2 >= paddle_y && 
-            x >= paddle_x-PADDLE_W/2 && 
-            x <= paddle_x+PADDLE_W/2)
+        // if ball has impacted human paddle then
+        // bounce ball off of human paddle
+        // xxx maybe use ball_right and ball_left
+        double ball_bottom        = y + BALL_RADIUS;
+        double ball_bottom_last   = y_last + BALL_RADIUS;
+        double human_paddle_top   = human_paddle_y - (PADDLE_H/2);
+        double human_paddle_left  = human_paddle_x - PADDLE_W/2;
+        double human_paddle_right = human_paddle_x + PADDLE_W/2;
+        if (ball_bottom >= human_paddle_top && 
+            ball_bottom_last < human_paddle_top &&
+            x >= human_paddle_left &&
+            x <= human_paddle_right)
         {
-            play_tone(1000,100);
-            paddle();
+            bounce_ball_off_paddle(true);
         }
 
-        computer_paddle_x = x;
-
-        // bounce off of computer_paddle
-        if (y_last-SIZE/2 > computer_paddle_y && 
-            y-SIZE/2 <= computer_paddle_y && 
-            x >= computer_paddle_x-PADDLE_W/2 && 
-            x <= computer_paddle_x+PADDLE_W/2)
+        // if ball has impacted computer paddle then
+        // bounce ball off of computer paddle
+        double ball_top               = y - BALL_RADIUS;
+        double ball_top_last          = y_last - BALL_RADIUS;
+        double computer_paddle_bottom = computer_paddle_y + (PADDLE_H/2);
+        double computer_paddle_left   = computer_paddle_x - PADDLE_W/2;
+        double computer_paddle_right  = computer_paddle_x + PADDLE_W/2;
+        if (ball_top <= computer_paddle_bottom && 
+            ball_top_last > computer_paddle_bottom &&
+            x >= computer_paddle_left &&
+            x <= computer_paddle_right)
         {
-            play_tone(1000,100);
-            computer_paddle();
+            bounce_ball_off_paddle(false);
         }
 
+        // if ball is above or below the display area then 
+        //   update score
+        //   play tone
+        //   set serve_needed flag
+        // endif
+        if (y < y_top || y > y_bottom) {
+            if (y < y_top) {
+                human_score++; 
+            } else {
+                computer_score++;
+            }
+            play_tone(500,250);
+            serve_needed = true;
+        }
 
-        // paddle
-        sdlx_render_fill_rect(paddle_x-PADDLE_W/2, paddle_y-PADDLE_H/2, PADDLE_W, PADDLE_H, COLOR_WHITE);
-
-        // computer_paddle
+        // display the ball and paddles
+        sdlx_render_texture(x-BALL_RADIUS, y-BALL_RADIUS, 2*BALL_RADIUS, 2*BALL_RADIUS, ball);
+        sdlx_render_fill_rect(human_paddle_x-PADDLE_W/2, human_paddle_y-PADDLE_H/2, PADDLE_W, PADDLE_H, COLOR_WHITE);
         sdlx_render_fill_rect(computer_paddle_x-PADDLE_W/2, computer_paddle_y-PADDLE_H/2, PADDLE_W, PADDLE_H, COLOR_WHITE);
 
+        // display scores
         sdlx_render_printf(0, 0, "%2d", computer_score);
         sdlx_render_printf(sdlx_win_width-2*sdlx_char_width, 0, "%d", human_score);
 
+        // register events
         sdlx_register_event(NULL, EVID_MOTION);
-
-        // register control event to
-        // - end program
         sdlx_register_control_events(NULL, NULL, "X", COLOR_WHITE, COLOR_BLACK, 0, 0, EVID_QUIT);
-
-
-
-/* INPROG
-move paddle
-- limit motion to x span
-bounce bottom of ball off paddle
-*/
-
-/*
-computer paddle
-score keeping
-sound effects, google search for positive and negative effects
-speed up ball gradually
-
-MAYBE LATER
-initial speed control
-
-IN PROG
-auto serve after 1 sec
-bounce ball off paddle in varying direction
-circular ball
-make ball and paddle different colors
-*/
 
         // present the display
         sdlx_display_present();
 
-        // wait for event, timeout=INTERVAL usecs
-        sdlx_get_event(INTERVAL, &event);
+        // wait for event with timeout
+        // xxx motion affects interval
+        sdlx_get_event(UPDATE_INTERVAL_USEC, &event);
 
         // process events
         switch (event.event_id) {
         case EVID_QUIT:
-            done = true;
+            end_program = true;
             break;
         case EVID_MOTION:
-            paddle_x += event.u.motion.xrel;
+            human_paddle_x += event.u.motion.xrel;
             break;
         }
     }
@@ -203,20 +226,28 @@ make ball and paddle different colors
     return 0;
 }
 
-#define DEG2RAD (M_PI/180.0)
-#define MAX_DEG 10.0
+// -----------------  SUPPORT  --------------------
 
-void paddle(void)
+void computer_paddle_control(void)
 {
-    //vy = -vy;
+    // xxx this may be too simple
+    computer_paddle_x = x;
+}
 
-    double vx1, vy1;
+// xxx comments
+#define MAX_RAD (10.0 * DEG2RAD)
+void bounce_ball_off_paddle(bool is_human_paddle)
+{
+    double vx1, vy1, theta;
+    double paddle_x;
 
-    double theta = 10 * DEG2RAD;
-
-    theta = (double)(paddle_x - x) / (PADDLE_W/2) * MAX_DEG;
-    printf("theta %0.2f\n", theta);
-    theta = theta * DEG2RAD;
+    if (is_human_paddle) {
+        paddle_x = human_paddle_x;
+        theta = (paddle_x - x) / (PADDLE_W/2) * MAX_RAD;
+    } else {
+        paddle_x = computer_paddle_x;
+        theta = -(paddle_x - x) / (PADDLE_W/2) * MAX_RAD;
+    }
 
     vx1 = vx*cos(theta) - vy*sin(theta);
     vy1 = vx*sin(theta) + vy*cos(theta);
@@ -224,22 +255,18 @@ void paddle(void)
     vy1 = -vy1;
 
     theta = -theta;
-
     vx = vx1*cos(theta) - vy1*sin(theta);
     vy = vx1*sin(theta) + vy1*cos(theta);
+
+    play_tone(1000,100);
 }
 
-void computer_paddle(void)
-{
-    vy = -vy;
-}
-
-void play_tone(int freq, int intvl_ms)
+void play_tone(int freq, int duration_ms)
 {
     static sdlx_tone_t t[2];
 
     t[0].freq = freq;
-    t[0].intvl_ms = intvl_ms;
-    
+    t[0].intvl_ms = duration_ms;
     sdlx_audio_play_tones(t);
 }
+
