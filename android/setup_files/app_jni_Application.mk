50c031a8ff0fdd8d652d5759001754db65ae46bf
Steven Haid <stevenhaid@gmail.com>
2025-12-22 07:21:42 -0500

    cont

--- android/setup_files/app_jni_Application.mk
+++ android/setup_files/app_jni_Application.mk
@@ -7,8 +7,11 @@
 APP_ABI := armeabi-v7a arm64-v8a x86 x86_64
 
 # Min runtime API level
-APP_PLATFORM=android-29
+APP_PLATFORM=android-29  // EZAPP
 
 # EZAPP
-# https://developer.android.com/guide/practices/page-sizes#update-packaging
+# - https://developer.android.com/guide/practices/page-sizes#update-packaging
+# - Google AI says: 
+#     "needed for Android apps that use native code (NDK) to be compatible with 
+#      future Android devices running Android 15+ which use 16 KB memory page sizes."
 APP_SUPPORT_FLEXIBLE_PAGE_SIZES := true
